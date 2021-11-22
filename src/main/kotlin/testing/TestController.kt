81245df98a862590c31bf43ce87e39b089c1c742
Leo Tietz <leo.tietz@gmail.com>
2021-11-22 21:30:44 +0000

    Add some more stuff

---
+++ src/main/kotlin/testing/TestController.kt
@@ -0,0 +1,15 @@
+package testing
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("test")
+class TestController {
+
+    @GetMapping("test")
+    fun test(): String {
+        return "hello"
+    }
+}
